cebc7bfaee283a866a9e54717bdb5a5f975a667d
Matthew Hartman <mattandalissa@gmail.com>
2018-11-22 06:10:32 -0500

    config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`]
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/path/to/markdown/files`,
+        name: "markdown-pages"
+      }
+    }
+  ]
 };
